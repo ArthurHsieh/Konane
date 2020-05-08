2d6e5861a8f5a7e962b02c6de3db93ccf6f47fa6
ArthurHsiehGamesofa <arthur.hsieh@gamesofa.com>
2020-05-08 11:47:24 +0800

    Node紀錄上下左右的Index

--- Assets/Scripts/Model/Node.cs
+++ Assets/Scripts/Model/Node.cs
@@ -11,6 +11,26 @@ namespace Model
             Index = index;
             X = index % boardSize;
             Y = index / boardSize;
+
+            if (X != boardSize - 1)
+                RightIndex = Index + 1;
+            else
+                RightIndex = -1;
+
+            if(X != 0)
+                LeftIndex = Index - 1;
+            else
+                LeftIndex = -1;
+
+            if (Y != 0)
+                UpIndex = Index - boardSize;
+            else
+                UpIndex = -1;
+
+            if (Y != boardSize - 1)
+                DownIndex = Index + boardSize;
+            else
+                DownIndex = -1;
         }
 
         public int Index { get; }
@@ -18,5 +38,9 @@ namespace Model
         public int Y { get; }
         public bool IsWhite => (X + Y) % 2 != 0;
         public bool IsVacancy;
+        public int RightIndex { get; }
+        public int LeftIndex { get; }
+        public int UpIndex { get; }
+        public int DownIndex { get; }
     }
 }
